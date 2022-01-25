fe150f880dae33d3721c198ff14117028db51c6b
Andriie <andrey137217@gmail.come.com>
2022-01-25 22:16:37 +0200

    Toolbar has been changed

--- app/src/main/java/com/example/testapplampa/MainActivity.kt
+++ app/src/main/java/com/example/testapplampa/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.testapplampa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.testapplampa.R
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +17,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter:MyAdapter
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
+    //private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //toolbar = findViewById(R.id.toolBar)
+        //toolbar.inflateMenu(R.menu.menu)
 
         vm = ViewModelProvider(this).get(ViewModelMain::class.java)
 
@@ -51,5 +58,11 @@ class MainActivity : AppCompatActivity() {
         }.attach()
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
